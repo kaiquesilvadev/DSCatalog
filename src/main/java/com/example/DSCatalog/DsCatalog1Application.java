81a337b7134c77a4c8e5f04db3ad36356e1e2080
kaiquesilvadev <j.kaique1570@Gmail.com>
2024-01-13 20:15:09 -0300

    inicio 0.0

---
+++ src/main/java/com/example/DSCatalog/DsCatalog1Application.java
@@ -0,0 +1,13 @@
+package com.example.DSCatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatalog1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatalog1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DSCatalog/DsCatalog1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DSCatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalog1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
