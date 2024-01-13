9ddd9793410dacc21bd2b40ec54250cd276ab292
kaiquesilvadev <j.kaique1570@Gmail.com>
2024-01-13 20:44:21 -0300

    mapeamento de muitos pra muitos

--- src/main/java/com/example/DSCatalog/domain/entities/User.java
+++ src/main/java/com/example/DSCatalog/domain/entities/User.java
@@ -1,8 +1,14 @@
 package com.example.DSCatalog.domain.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -24,4 +30,10 @@ public class User {
 	private String vlastName;
 	private String email;
 	private String password;
+
+	@ManyToMany
+	@JoinTable(name = "tb_user_role" ,
+	joinColumns = @JoinColumn(name = "user_id") ,
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
 }
