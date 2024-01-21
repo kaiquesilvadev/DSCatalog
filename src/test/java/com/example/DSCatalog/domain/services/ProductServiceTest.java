d29b77fb4c0b629f0e290670db1a74fa63ce57e6
kaiquesilvadev <j.kaique1570@Gmail.com>
2024-01-20 21:41:07 -0300

    teste unitario de delete em product quando passado id existente que esta em associação com outra tabela

--- src/test/java/com/example/DSCatalog/domain/services/ProductServiceTest.java
+++ src/test/java/com/example/DSCatalog/domain/services/ProductServiceTest.java
@@ -2,6 +2,7 @@ package com.example.DSCatalog.domain.services;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
@@ -13,9 +14,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.example.DSCatalog.domain.entities.Product;
+import com.example.DSCatalog.domain.exception.EntidadeEmUsoException;
 import com.example.DSCatalog.domain.exception.ProductNaoEncontradoException;
 import com.example.DSCatalog.domain.repositories.ProductRepository;
 
@@ -29,22 +32,26 @@ public class ProductServiceTest {
 	private  ProductRepository repository;
 	private  Long idExistente;
 	private Long idInexistente;
+	private Long idAssociado;
 	
 	@BeforeEach
 	private void setUp() throws Exception{
 		idExistente = 2L;
 		idInexistente = 100L;
+		idAssociado = 3L;
 		// Configuração do mock para o método findById
 		// Quando o método findById do mock repository for chamado com o argumento idExistente,
 		// então retorne um Optional contendo um objeto Product simulado.
 		when(repository.findById(idExistente)).thenReturn(Optional.of( new Product()));
+		when(repository.findById(idAssociado)).thenReturn(Optional.of( new Product()));
 		
 		// Configuração do mock para o método deleteById
 		// Quando o método deleteById do mock repository for chamado com o argumento idExistente,
 		// não faça nada (doNothing), pois é um método void (não retorna valor).
-		Mockito.doNothing().when(repository).deleteById(idExistente);
-		
-		when(repository.findById(idInexistente)).thenThrow(new ProductNaoEncontradoException(idExistente));
+		doNothing().when(repository).deleteById(idAssociado);
+		Mockito.doThrow(DataIntegrityViolationException.class).when(repository).deleteById(idAssociado);
+
+		when(repository.findById(idInexistente)).thenThrow(new ProductNaoEncontradoException(idInexistente));
 	}
 	
 	@Test
@@ -67,6 +74,15 @@ public class ProductServiceTest {
 
 		assertThrows(ProductNaoEncontradoException.class, () -> {
 			service.deleta(idInexistente);
+		});
+	}
+	
+	@Test
+	@DisplayName("testa se retorna uma exception se passar um id que esteja associado a outra tabela")
+	public void testaSeGeraUmaExceptionQuandoOIdEstiverAssociadoAOutraTabela() {
+
+		assertThrows(EntidadeEmUsoException.class, () -> {
+			service.deleta(idAssociado);
 		});
 	}
 }
