cae5cb89a577fd5efdb5a7abcacb85a4eb1705b3
kaiquesilvadev <j.kaique1570@gmail.com>
2024-02-01 18:38:12 -0300

    authorization server , resouce server

--- src/main/java/com/example/DSCatalog/domain/services/UserSecurityService.java
+++ src/main/java/com/example/DSCatalog/domain/services/UserSecurityService.java
@@ -4,10 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.example.DSCatalog.domain.entities.User;
 import com.example.DSCatalog.domain.repositories.UserRepository;
 
+@Service
 public class UserSecurityService implements UserDetailsService{
 	
 	@Autowired
