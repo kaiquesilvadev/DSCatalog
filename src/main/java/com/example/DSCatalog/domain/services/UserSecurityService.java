a5286f8b733287596048129b1eb5fd5ed8dce2bc
kaiquesilvadev <j.kaique1570@gmail.com>
2024-02-10 19:22:25 -0300

    Obtendo usuário logado

--- src/main/java/com/example/DSCatalog/domain/services/UserSecurityService.java
+++ src/main/java/com/example/DSCatalog/domain/services/UserSecurityService.java
@@ -1,28 +1,42 @@
 package com.example.DSCatalog.domain.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Service;
 
 import com.example.DSCatalog.domain.entities.User;
 import com.example.DSCatalog.domain.repositories.UserRepository;
 
 @Service
-public class UserSecurityService implements UserDetailsService{
-	
+public class UserSecurityService implements UserDetailsService {
+
 	@Autowired
 	private UserRepository userRepository;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+
 		User user = userRepository.validaEmail(username);
-		
-		if(user == null)
+
+		if (user == null)
 			throw new UsernameNotFoundException("User not found");
 		return user;
+	}
+
+	protected User authenticated() {
+		try {
+			Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+			Jwt jwtPrincipal = (Jwt) authentication.getPrincipal();
+			String username = jwtPrincipal.getClaim("username");
+			return userRepository.findByEmail(username);
+		} catch (Exception e) {
+			throw new UsernameNotFoundException("Invalid user");
+		}
 	}
 
 }
